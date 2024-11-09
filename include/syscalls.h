7f0e51eac0ae7252a4b8cf01a67f3c3e7dda9cf8
kellerm2 <kellerm2@purdue.edu>
2024-11-09 14:19:37 -0500

    idk if we do header files

---
+++ include/syscalls.h
@@ -0,0 +1,21 @@
+/* Includes */
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/times.h>
+
+
+/* Variables */
+//#undef errno
+extern int errno;
+extern int __io_putchar(int ch) __attribute__((weak));
+extern int __io_getchar(void) __attribute__((weak));
+
+register char * stack_ptr asm("sp");
+
+char *__env[1] = { 0 };
+char **environ = __env;
