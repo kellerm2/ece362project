ba0b20151b9423de3a6e3c4c8fe70bd68ed64ca2
kellerm2 <kellerm2@purdue.edu>
2024-11-09 15:22:18 -0500

    Need to put all parts of files into projectcode, and tie them together

--- src/projectcode.c
+++ src/projectcode.c
@@ -3,6 +3,26 @@
 #include <stdint.h>
 #include <stdio.h>
 
+// #include "spi_tft.h"
+// #include "audio_input.h"
+// #include "dma_handler.h"
+// #include "support.h"
+// #include "syscalls.h"
+// #include "display_control.h"
+// #include "graphics.h"
+
+// SETUP HERE - ENABLING STUFF
+
+// AUDIO INPUT HERE
+
+// DMA HANDLER HERE
+
+// SPI TFT HERE
+
+// GRAPHICS HERE
+
+// DISPLAY CTRL HERE
+// SETUP HERE - ENABLING STUFF
 #include "spi_tft.h"
 #include "audio_input.h"
 #include "dma_handler.h"
@@ -10,4 +30,20 @@
 #include "syscalls.h"
 #include "display_control.h"
 #include "graphics.h"
+
+// ENABLE GPIO AND STUFF
+
+// AUDIO INPUT HERE
+
+
+// DMA HANDLER HERE
+
+
+// SPI TFT HERE
+
+
+// GRAPHICS HERE
+
+
+// DISPLAY CTRL HERE
 
